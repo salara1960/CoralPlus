7234b2404104c99dbe6cda32a975a247c084e740
salara1960 <a.ilminsky@gmail.com>
2019-11-04 13:15:44 +0200

    Minor changes : now two installer present - Linux_x64 and Win32

---
+++ data/linux/mk_run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+#  used makeself.sh script from project https://github.com/megastep/makeself.git
+
+makeself.sh --bzip2 --notemp ./dist-x64 out/CoralPlusInst.run "SalaraSoft" echo "CoralPlus has extracted itself"
